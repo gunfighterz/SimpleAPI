1c24420e2e7bdd041b18069a7cfe3ebc2179156f
Jamie Dumont <JDumont@pcl.com>
2020-08-08 13:33:50 -0600

    Initial Configuration Complete

--- test/SimpleAPI.TEST/UnitTest1.cs
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -1,10 +1,18 @@
 using System;
 using Xunit;
+using SimpleAPI.Controllers;
 
 namespace SimpleAPI.TEST
 {
     public class UnitTest1
     {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Jamie Dumont", returnValue.Value);
+        }
         [Fact]
         public void Test1()
         {
